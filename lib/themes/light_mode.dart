ed827311cf9bbdaac565dfe0be6901512acd14b8
Milan Koirala <koiralamilon67@gmail.com>
2025-06-26 23:41:10 +0545

    Set up custom light theme for the app

---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade300,
+    primary: Colors.grey.shade200,
+    secondary: Colors.white,
+    inversePrimary: Colors.grey.shade700,
+  ),
+  scaffoldBackgroundColor: Colors.grey.shade300,
+  useMaterial3: true,
+);
