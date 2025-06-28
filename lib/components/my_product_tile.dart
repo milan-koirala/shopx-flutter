f52243eed5796382ad1b4816b100a5f51164a046
Milan Koirala <koiralamilon67@gmail.com>
2025-06-28 20:48:00 +0545

    Update design product tile UI with styling, layout, and price display

--- lib/components/my_product_tile.dart
+++ lib/components/my_product_tile.dart
@@ -12,19 +12,66 @@ class MyProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(25),
+      width: 300,
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          // product image
-          Icon(Icons.favorite),
+          Column(
+            children: [
+              // product image
+              AspectRatio(
+                aspectRatio: 1,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.secondary,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(25),
+                  child: const Icon(Icons.favorite)
+                ),
+              ),
 
-          // product name
-          Text(product.name),
+              const SizedBox(height: 25),
 
-          // product description
-          Text(product.description),
+              // product name
+              Text(
+                product.name,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+
+                const SizedBox(height: 10),
+
+              // product description
+              Text(
+                product.description,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 25),
 
           // product price ra add to cart button 
-          Text(product.price.toStringAsFixed(2)),
+          Row(
+            children: [
+              // product price
+              Text("\Rs. " + product.price.toStringAsFixed(2)),
+              
+            ],
+          ),
         ],
       ),
     );
