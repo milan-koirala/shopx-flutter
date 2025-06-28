ec430faf9fe0771374134012376ac23e92eec4ad
Milan Koirala <koiralamilon67@gmail.com>
2025-06-29 00:40:47 +0545

    - Replaced static favorite icon with Image.asset displaying product.imagePath

--- lib/components/my_product_tile.dart
+++ lib/components/my_product_tile.dart
@@ -64,7 +64,7 @@ class MyProductTile extends StatelessWidget {
                   ),
                   width: double.infinity,
                   padding: const EdgeInsets.all(25),
-                  child: const Icon(Icons.favorite),
+                  child: Image.asset(product.imagePath),
                 ),
               ),
 
