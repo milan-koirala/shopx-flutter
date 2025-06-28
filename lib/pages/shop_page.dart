6ed74a956a31a0debf78fa9cbcb6cdec9c591524
Milan Koirala <koiralamilon67@gmail.com>
2025-06-28 20:52:02 +0545

    Display product list on Shop Page using Provider and MyProductTile

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shopx_flutter/components/my_drawer.dart';
+import 'package:shopx_flutter/components/my_product_tile.dart';
+import 'package:shopx_flutter/models/shop.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // acess products in shop
+    final products = context.watch<Shop>().shop;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -15,6 +21,38 @@ class ShopPage extends StatelessWidget {
       ),
       drawer: const MyDrawer(),
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: ListView(
+        children: [
+          const SizedBox(height: 25),
+
+          // shop subtitle
+          Center(
+            child: Text(
+              "Pick from a selected list of premium products",
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.inversePrimary
+              ),
+            ),
+          ),
+
+          // product list
+          SizedBox(
+            height: 550,
+            child: ListView.builder(
+              itemCount: products.length,
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(10),
+              itemBuilder: (context, index) {
+                // get each individual product from shop
+                final product = products[index];
+
+                // return as a product tile UI
+                return MyProductTile(product: product);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
