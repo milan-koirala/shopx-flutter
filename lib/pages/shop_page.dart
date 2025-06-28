55a8def2e72e35fdba39dec07e8a737aaf31336d
Milan Koirala <koiralamilon67@gmail.com>
2025-06-28 23:54:59 +0545

    Add cart navigation button to ShopPage AppBar

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -18,6 +18,12 @@ class ShopPage extends StatelessWidget {
         elevation: 0,
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Shop Page"),
+        actions: [
+          // go to cart button
+          IconButton(
+            onPressed: () => Navigator.pushNamed(context, "/cart_page"),
+            icon: const Icon(Icons.shopping_cart_outlined))
+        ],
       ),
       drawer: const MyDrawer(),
       backgroundColor: Theme.of(context).colorScheme.background,
