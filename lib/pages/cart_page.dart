b183b3f674fe558131edbd6339ccf27c5d167879
Milan Koirala <koiralamilon67@gmail.com>
2025-06-28 23:53:51 +0545

    Implement cart UI with item removal and payment confirmation dialogs

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,10 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shopx_flutter/components/my_button.dart';
+import 'package:shopx_flutter/models/product.dart';
+import 'package:shopx_flutter/models/shop.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  // method of remove item from cart
+  void removeItemFromCart(BuildContext context, Product product) {
+    // show to dialog box to ask user to confirm to remove item from cart
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: const Text("Remove this item from your cart?"),
+        actions: [
+          // cancel button
+          MaterialButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Cancel"),
+          ),
+
+          // yes button
+          MaterialButton(
+            onPressed: () {
+              // pop dialog box
+              Navigator.pop(context);
+
+              // add to cart
+              context.read<Shop>().addToCart(product);
+            },
+            child: Text("Yes"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // user pressed the pay button
+  void payButtonPressed(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => const AlertDialog(
+        content: Text(
+          "User want to pay! Connect this app to your payment backend",
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    // get access to the cart
+    final cart = context.watch<Shop>().cart;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -13,6 +62,41 @@ class CartPage extends StatelessWidget {
         title: Text("Cart Page"),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        children: [
+          // card list
+          Expanded(
+            child: cart.isEmpty
+                ? Center(child: Text("Your cart is empty.."))
+                : ListView.builder(
+                    itemCount: cart.length,
+                    itemBuilder: (context, index) {
+                      // get individual item in cart
+                      final item = cart[index];
+
+                      // return as a cart tile UI
+                      return ListTile(
+                        title: Text(item.name),
+                        subtitle: Text(item.price.toStringAsFixed(2)),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.remove),
+                          onPressed: () => removeItemFromCart(context, item),
+                        ),
+                      );
+                    },
+                  ),
+          ),
+
+          // pay button
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: MyButton(
+              onTap: () => payButtonPressed(context),
+              child: Text("PAY NOW"),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
