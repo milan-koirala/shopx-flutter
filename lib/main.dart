bcf30ff96a27c1e3f6abc7c9fcde6d16e652fab4
Milan Koirala <koiralamilon67@gmail.com>
2025-06-29 23:48:11 +0545

    Add theme switching support and settings page route

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,18 @@ import 'package:provider/provider.dart';
 import 'package:shopx_flutter/models/shop.dart';
 import 'package:shopx_flutter/pages/cart_page.dart';
 import 'package:shopx_flutter/pages/intro_page.dart';
+import 'package:shopx_flutter/pages/settings_page.dart';
 import 'package:shopx_flutter/pages/shop_page.dart';
 import 'package:shopx_flutter/themes/light_mode.dart';
+import 'package:shopx_flutter/themes/theme_provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(
-      create: (context) => Shop(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => Shop()),
+      ],
       child: const MyApp(),
     ),
   );
@@ -23,11 +28,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: IntroPage(),
-      theme: lightMode,
+      theme: Provider.of<ThemeProvider>(context).themeData,
       routes: {
-        "/intro_page":(context) => const IntroPage(),
-        "/shop_page":(context) => const ShopPage(),
-        "/cart_page" :(context) => const CartPage(),
+        "/intro_page": (context) => const IntroPage(),
+        "/shop_page": (context) => const ShopPage(),
+        "/cart_page": (context) => const CartPage(),
+        "/setting_page": (context) => SettingsPage(),
       },
     );
   }
