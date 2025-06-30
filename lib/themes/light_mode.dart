fcafaab9230a6d2077777d7e7ce7614d464186eb
Milan Koirala <koiralamilon67@gmail.com>
2025-06-30 21:39:53 +0545

    Update light mode color scheme with new shades

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-    background: Colors.grey.shade300,
-    primary: Colors.grey.shade200,
-    secondary: Colors.white,
-    inversePrimary: Colors.grey.shade700,
+    background: Colors.grey.shade100,
+    primary: Colors.grey.shade300,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.grey.shade50,
+    inversePrimary: Colors.grey.shade900,
   ),
-  scaffoldBackgroundColor: Colors.grey.shade300,
-  useMaterial3: true,
 );
