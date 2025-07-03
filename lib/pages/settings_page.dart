9b28ca4dfb110683146e58964de6bb4439e18411
Milan Koirala <koiralamilon67@gmail.com>
2025-07-03 23:42:22 +0545

    Add list as change language setting with icons

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -16,26 +16,71 @@ class SettingsPage extends StatelessWidget {
         foregroundColor: Colors.grey,
         elevation: 0,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.secondary,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        margin: const EdgeInsets.all(25),
-        padding: const EdgeInsets.all(16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            // dark mode
-            const Text("Dark Mode"),
-        
-            // switch toggle
-            CupertinoSwitch(
-              value: Provider.of<ThemeProvider>(context, listen: false).isDarkMode,
-              onChanged: (value) => Provider.of<ThemeProvider>(context, listen: false).toggleTheme(),
+      body: Column(
+        children: [
+          // Dark Mode Toggle
+          Container(
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.secondary,
+              borderRadius: BorderRadius.circular(8),
             ),
-          ],
-        ),
+            margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 12),
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                    children: [
+                      Icon(Icons.dark_mode),
+                      SizedBox(width: 8),
+                      const Text("Dark Mode"),
+                    ],
+                  ),
+                CupertinoSwitch(
+                  value: Provider.of<ThemeProvider>(
+                    context,
+                    listen: false,
+                  ).isDarkMode,
+                  onChanged: (value) {
+                    Provider.of<ThemeProvider>(
+                      context,
+                      listen: false,
+                    ).toggleTheme();
+                  },
+                ),
+              ],
+            ),
+          ),
+
+          // Change Language Container
+          GestureDetector(
+            onTap: () => Navigator.pushNamed(
+              context,
+              "/language_page",
+            ), // redirect to language select page
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              margin: const EdgeInsets.symmetric(horizontal: 25, vertical: 12),
+              padding: const EdgeInsets.all(16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Row(
+                    children: [
+                      Icon(Icons.language),
+                      SizedBox(width: 8),
+                      Text("Change Language"),
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios, size: 16),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
