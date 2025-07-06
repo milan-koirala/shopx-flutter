55fa9d06478905f1f524e36b4dce99db5be7064a
Milan Koirala <koiralamilon67@gmail.com>
2025-07-06 21:32:18 +0545

    Update darkMode and add matching lightMode color scheme

--- lib/themes/dark_mode.dart
+++ lib/themes/dark_mode.dart
@@ -5,7 +5,7 @@ ThemeData darkMode = ThemeData(
     background: Colors.grey.shade900,
     primary: Colors.grey.shade600,
     secondary: const Color.fromARGB(255, 57, 57, 57),
-    tertiary: Colors.grey.shade800,
-    inversePrimary: Colors.grey.shade300,
+    tertiary: Colors.grey.shade400,
+    inversePrimary: Colors.grey.shade200,
   ),
 );
