c3f9ee57e3b65278c1b1b9e6af38df6d837826c9
Milan Koirala <koiralamilon67@gmail.com>
2025-07-06 21:33:27 +0545

    Refine lightMode secondary color to custom light gray

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -5,7 +5,7 @@ ThemeData lightMode = ThemeData(
   colorScheme: ColorScheme.light(
     background: Colors.grey.shade100,
     primary: Colors.grey.shade300,
-    secondary: Colors.grey.shade200,
+  secondary: const Color.fromARGB(255, 230, 230, 230),
     tertiary: Colors.grey.shade50,
     inversePrimary: Colors.grey.shade900,
   ),
